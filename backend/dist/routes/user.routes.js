a6e2e9adba6c1d8e0a80a518b0ac57ea8f321f23
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2025-06-19 04:05:40 -0400

    build: setting electron

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -1,1 +1,7 @@
-export {};
+import { Router } from "express";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
+const router = Router();
+// Rutas para usuarios
+router.post("/login", loginUser);
+router.post("/register", registerUser);
+export const userRoutes = router;
